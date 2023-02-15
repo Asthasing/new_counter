63bc202fe4f5a2fcec8511a61f9d24245840b35b
astha singh <92517275+Asthasing@users.noreply.github.com>
2023-02-15 20:41:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { React, useState } from 'react'
+import "./App.css"
+ 
+export default function App() {
+  const [counter, setCounter] = useState(0);
+ 
+  //increase counter
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+ 
+  //decrease counter
+  const decrease = () => {
+    setCounter(count => count - 1);
+  };
+ 
+  //reset counter 
+  const reset = () =>{
+    setCounter(0)
+  }
+ 
+  return (
+    <div className="counter">
+     
+     <h1 className='counter' style={{ color: counter < 5? 'green' :(counter>=5 && counter<10)?'blue':'red'}}>Counter</h1>
+      <span className="counter__output">{counter}</span>
+      <div className="btn__container">
+        <button className="control__btn" onClick={increase}>+</button>
+        <button className="control__btn" onClick={decrease}>-</button>
+        <button className="reset" onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
